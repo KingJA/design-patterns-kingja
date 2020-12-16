c12521a88262133f3f0579125d9197db2f083ff7
KingJA <kingjavip@gmail.com>
2020-12-16 15:59:34 +0800

    init

---
+++ src/main/java/com/kingja/designpatterns/patterns/structural/proxy/ProxyTest.java
@@ -0,0 +1,13 @@
+package com.kingja.designpatterns.patterns.structural.proxy;
+
+/**
+ * Description:TODO
+ * Create Time:2020/12/16 0016 15:59
+ * Author:KingJA
+ * Email:kingjavip@gmail.com
+ */
+public class ProxyTest {
+    public static void main(String[] args) {
+
+    }
+}
